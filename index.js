3789cd2bcba96ffd5e12b65c7cf7e7e2120a5a5c
ol cher <cheremisinova8@gmail.com>
2021-07-25 22:19:40 +0300

    main

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import { fetchUsersList, fetchTasksList } from "./api.js"
+import { fetchUsersList, fetchTasksList } from "./api.js";
 
-const day_width = 200;
+const day_width = 150;
+let taskList = [];
+
+function toTimestamp(date){
+    date.setHours(0,0,0,0);
+    var datum = Date.parse(date);
+    return datum/1000;
+ }
 
 function createElementWithClassName(tag, className) {
     const element = document.createElement(tag);
@@ -14,12 +21,19 @@ function createUserRow(userId, userName) {
     user.textContent = userName;
     row.dataset.userId = userId;
     row.append(user);
-    const tasks = document.getElementById("tasks");
-    tasks.append(row);
+    const schedule = document.querySelector("#schedule");
+    schedule.append(row);
+
+    row.addEventListener("dragover", allowDrop);
+    row.addEventListener("drop", drop);
+}
+
+function allowDrop(event) {
+    event.preventDefault();
 }
 
 function deleteDefaultRow() {
-    const row = document.getElementById("default-row");
+    const row = document.querySelector("#default-row");
     if (row) {
         row.remove();
     }
@@ -37,26 +51,89 @@ async function renderUsers() {
         users.forEach((user) => {
             createUserRow(user.id, getUserName(user))
         });
-    } catch {
-        alert("Ошибка загрузки данных");
+    } catch (e) {
+        alert("Ошибка загрузки списка пользователей", e);
     }
 }
 
 function createBacklogTask(task) {
     const taskBlock = createElementWithClassName("div", "backlog-panel__task");
-    taskBlock.textContent = task.subject;
-    const container = document.getElementById("tasks-container");
+    const title = createElementWithClassName("span", "backlog-panel__task-title");
+    title.textContent = task.subject;
+    const content = createElementWithClassName("span", "backlog-panel__task-content");
+    content.textContent = task.description;
+    taskBlock.append(title);
+    taskBlock.append(content);
+    taskBlock.dataset.id = task.id;
+    taskBlock.draggable = true;
+    const container = document.querySelector("#tasks-container");
     container.append(taskBlock);
+
+    taskBlock.addEventListener("dragstart", drag);
+}
+
+function drag(event) {
+    event.dataTransfer.setData('id', event.target.dataset.id);
+}
+
+function drop(event) {
+    const id = event.dataTransfer.getData('id');
+    const target = event.target.closest(".user-row")
+    const task = taskList.find((item) => item.id.toString() === id.toString());
+    if (task) {
+        createUserTask({ ...task, executor: target.dataset.userId });
+        updateTaskDate();
+        deleteBacklogTask(id);
+    }
+}
+
+function deleteBacklogTask(id) {
+    const backlogTasks = document.querySelectorAll(".backlog-panel__task");
+    for (let i = 0; i < backlogTasks.length; i++) {
+        if (backlogTasks[i].dataset.id.toString() === id.toString()) {
+            backlogTasks[i].remove();
+        }
+    }
+}
+
+function updateTaskDate() {
+    const days = document.querySelectorAll(".tasks-panel__day");
+    const tasks = document.querySelectorAll(".tasks-panel__tasks-wrapper");
+    tasks.forEach((task) => {
+        const idx = Array.prototype.findIndex.call(days, (item) => item.dataset.date == task.dataset.planStartDate);
+                
+        if (idx >-1) {
+            task.style.left = idx * day_width + 100 + "px";
+            task.style.display = "flex";
+        } else {
+            task.style.display = "none";
+        }
+    });
+    
 }
 
 function createUserTask(task) {
     const taskBlock = createElementWithClassName("div", "tasks-panel__task");
     taskBlock.textContent = task.subject;
+    const tooltip = createElementWithClassName("span", "tooltip");
+    tooltip.textContent = `Описание: ${task.description}\nДата создания: ${task.creationDate}\nДата начала выполнения: ${task.planStartDate}\nДата окончания выполнения: ${task.planEndDate}`;
+    taskBlock.append(tooltip);
+    
     const containers = document.querySelectorAll(".user-row");
+
     for (let i = 0; i < containers.length; i++) {
         const container = containers[i];
         if (container.dataset.userId.toString() === task.executor.toString()) {
-            container.append(taskBlock);
+            const existsDays = container.querySelectorAll(".tasks-panel__tasks-wrapper");
+            const taskDay = Array.prototype.find.call(existsDays, (item) => item.dataset.planStartDate == toTimestamp(new Date(task.planStartDate)));
+            if (taskDay) {
+                taskDay.append(taskBlock);
+            } else {
+                const dayWrapper = createElementWithClassName("div", "tasks-panel__tasks-wrapper");
+                dayWrapper.dataset.planStartDate = toTimestamp(new Date(task.planStartDate));
+                dayWrapper.append(taskBlock);
+                container.append(dayWrapper);
+            }
             break;
         }
     }
@@ -64,21 +141,22 @@ function createUserTask(task) {
 
 async function renderTasks() {
     try {
-        const tasks = await fetchTasksList();
-        tasks.forEach((task) => {
+        taskList = await fetchTasksList();
+        taskList.forEach((task) => {
             if (task.executor === null) {
                 createBacklogTask(task);
             } else {
                 createUserTask(task);
             }
         });
-    } catch {
-        alert("Ошибка загрузки данных");
+        updateTaskDate();
+    } catch (e) {
+        alert("Ошибка загрузки списка задач", e);
     }
 }
 
 function createCalendar() {
-    const container = document.getElementById("calendar");
+    const container = document.querySelector("#calendar");
     let num = Math.floor((container.clientWidth - 100) / 200);
     num = Math.max(num, 7);
     for (let i = 0; i < num; i++) {
@@ -86,12 +164,65 @@ function createCalendar() {
         date.setDate(date.getDate() + i);
         const dateBlock = createElementWithClassName("div", "tasks-panel__day");
         dateBlock.textContent = date.toLocaleDateString();
+        dateBlock.dataset.date = toTimestamp(date);
         container.append(dateBlock);
     }
+}
+
+function shiftCalendar() {
+    let days_shift = 0;
+    
+    return function(shift) {
+        days_shift += shift;
+        const days = document.querySelectorAll(".tasks-panel__day");
+        days.forEach((day, idx) => {
+            const date = new Date();
+            date.setDate(date.getDate() + idx + days_shift);
+            day.textContent = date.toLocaleDateString();
+            day.dataset.date = toTimestamp(date);
+        });
+        updateTaskDate();
+    }
+}
+
+function search(e) {
+    e.preventDefault();
+    const searchBar = document.querySelector(".search");
+    const container = document.querySelector("#tasks-container");
+    container.innerHTML = '';
+    taskList.forEach((task) => {
+        if (task.executor === null && task.subject.toLowerCase().indexOf(searchBar.value.toLowerCase()) !== -1) {
+            createBacklogTask(task);
+        }
+    });
 }
 
 window.onload = async function() {
     createCalendar();
     await renderUsers();
     renderTasks();
+    const prev = document.querySelector("#prev-button");
+    const next = document.querySelector("#next-button");
+    const shift = shiftCalendar();
+    prev.addEventListener("click", () => shift(-1));
+    next.addEventListener("click", () => shift(1));
+
+    const clearIcon = document.querySelector(".clear-icon");
+    const searchBar = document.querySelector(".search");
+
+    searchBar.addEventListener("keyup", () => {
+        if(searchBar.value && clearIcon.style.visibility != "visible"){
+            clearIcon.style.visibility = "visible";
+        } else if(!searchBar.value) {
+            clearIcon.style.visibility = "hidden";
+        }
+    });
+
+    clearIcon.addEventListener("click", () => {
+        searchBar.value = "";
+        clearIcon.style.visibility = "hidden";
+    })
+
+    const searchRow = document.querySelector(".search-row");
+    searchRow.addEventListener("submit", search);
 };
